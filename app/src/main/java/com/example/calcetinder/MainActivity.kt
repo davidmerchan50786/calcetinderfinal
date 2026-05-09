23b46a687b8c4fbfdf02c50dfe511fd9f5fc17ea
davidmerchan50786 <makhnovia@gmail.com>
2026-05-09 16:33:27 +0200

    actividad principal

---
+++ app/src/main/java/com/example/calcetinder/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.calcetinder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.calcetinder.datos.Seeder
+import com.example.calcetinder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        Seeder.poblarSiVacio(this)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+    }
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
+}
